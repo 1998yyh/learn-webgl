6d7b8a09d9d9229b8c4658fe9af11c2edee59e8b
yanghao <532438158@qq.com>
2022-04-07 18:56:01 +0800

    【其他修改】webgl

--- util.js
+++ util.js
@@ -468,12 +468,31 @@ const m4 = {
     } else {
       return [0, 0, 0];
     }
+  },
+  transformPoint(m, v, dst) {
+    dst = dst || new Float32Array(3);
+    var v0 = v[0];
+    var v1 = v[1];
+    var v2 = v[2];
+    var d = v0 * m[0 * 4 + 3] + v1 * m[1 * 4 + 3] + v2 * m[2 * 4 + 3] + m[3 * 4 + 3];
+
+    dst[0] = (v0 * m[0 * 4 + 0] + v1 * m[1 * 4 + 0] + v2 * m[2 * 4 + 0] + m[3 * 4 + 0]) / d;
+    dst[1] = (v0 * m[0 * 4 + 1] + v1 * m[1 * 4 + 1] + v2 * m[2 * 4 + 1] + m[3 * 4 + 1]) / d;
+    dst[2] = (v0 * m[0 * 4 + 2] + v1 * m[1 * 4 + 2] + v2 * m[2 * 4 + 2] + m[3 * 4 + 2]) / d;
+
+    return dst;
   }
 }
 
 
+// 角度转弧度
 function degToRad(d) {
   return d * Math.PI / 180;
+}
+
+// 弧度转角度
+function radToDeg(d){
+  return r * 180 / Math.PI;
 }
 
 // 计算叉乘
